1a9ac06c25e4f6182e5c75686a5a07b4f3041b0a
tungnc <tungnc@kaopiz.com>
2018-07-12 17:05:43 +0700

    Merge branch 'master' of https://github.com/bkaprodx/ShopToy
    # Conflicts:
    #	.gitignore

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
+use App\Slide;
 
 class PageController extends Controller
 {
     public function getIndex()
     {
-        return view('master');
+        $slides = Slide::all();
+        $new_products = Product::where('new', 1)->paginate(4);
+        $promotion_products = Product::where('promotion_price', '<>', 0)->paginate(8);
+        return view('page.trangchu', compact('slides', 'new_products', 'promotion_products'));
+    }
+
+    public function getLoaiSp($type)
+    {
+        $sp_theoloai = Product::where('id_type', $type)->get();
+        $sp_khac = Product::where('id_type', '<>', $type)->paginate(3);
+        return view('page.loai_sanpham', compact('sp_theoloai', 'sp_khac'));
+    }
+
+    public function getChitiet()
+    {
+        return view('page.chitiet_sanpham');
+    }
+
+    public function getLienHe()
+    {
+        return view('page.lienhe');
+    }
+
+    public function getGioithieu()
+    {
+        return view('page.gioithieu');
     }
 }
